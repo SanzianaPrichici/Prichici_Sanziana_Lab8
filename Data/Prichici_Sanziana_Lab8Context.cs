74ef5ff1a8e7ed7245a5268dd14a49e1a26dd7cf
MARIA-SÂNZIANA PRICHICI <maria.prichici@stud.ubbcluj.ro>
2021-11-24 23:31:06 +0200

    Add project files.

---
+++ Data/Prichici_Sanziana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Prichici_Sanziana_Lab8.Models;
+
+namespace Prichici_Sanziana_Lab8.Data
+{
+    public class Prichici_Sanziana_Lab8Context : DbContext
+    {
+        public Prichici_Sanziana_Lab8Context (DbContextOptions<Prichici_Sanziana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Prichici_Sanziana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Prichici_Sanziana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Prichici_Sanziana_Lab8.Models.Category> Category { get; set; }
+    }
+}
